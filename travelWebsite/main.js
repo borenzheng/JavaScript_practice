04edf30e632d36e8c8aef7ffd4d9dff61308ab99
borenzheng <boren_zheng@uri.edu>
2019-10-29 17:53:21 -0400

    switch to new weather APT

--- travelWebsite/main.js
+++ travelWebsite/main.js
@@ -3,9 +3,13 @@ const clientId = '1WVDSNKKW15F1ZXJ54NLX43K4LIRJEMNW01EDJ5KLRC3XFCC';
 const clientSecret = 'ON0REKKBMV3MZRQW4BFBVFPBDDUVL4HZU4TPOCBESENBF1KU';
 const url = 'https://api.foursquare.com/v2/venues/explore?near=';
 
-// APIXU Info
-const apiKey = '75d278f0ca164ea7b56160902192804';
-const forecastUrl = 'https://api.apixu.com/v1/forecast.json?key=';
+// // APIXU Info
+// const apiKey = '75d278f0ca164ea7b56160902192804';
+// const forecastUrl = 'https://api.apixu.com/v1/forecast.json?key=';
+
+// OpenWeather API Info
+const openWeatherKey = '9adbd340dd1a85f8733a87b7ff44e3f2';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 // Page Elements
 const $input = $('#city');
@@ -13,10 +17,10 @@ const $submit = $('#button');
 const $destination = $('#destination');
 const $container = $('.container');
 const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
-const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4")];
+const $weatherDiv = $("#weather1");
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-// AJAX functions
+// Add AJAX functions here:
 const getVenues = async () => {
 const city = $input.val();
 const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20190428`;
@@ -40,14 +44,14 @@ const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=$
 }
 
 const getForecast = async () => {
-  const urlToFetch = `${forecastUrl}${apiKey}&q=${$input.val()}&days=4&$hour=11`; 
+  const urlToFetch = `${weatherUrl}?&q=${$input.val()}&APPID=${openWeatherKey}`; 
   try {
     const response = await fetch(urlToFetch);
     if(response.ok){
       const jsonResponse = await response.json();
       console.log(jsonResponse);
-      const days= jsonResponse.forecast.forecastday;
-      return days;
+      //const day= jsonResponse.forecast.forecastday;
+      return jsonResponse;
     }
   }catch(error){
     console.log(error);
@@ -62,23 +66,24 @@ const renderVenues = (venues) => {
     const venue = venues[index];
     const venueIcon = venue.categories[0].icon;
     const venueImgSrc = `${venueIcon.prefix}bg_64${venueIcon.suffix}`;
-    let venueContent = createVenueHTML(venue.name,venue.location,venueImgSrc);
+    let venueContent = createVenueHTML(venue.name, venue.location, venueImgSrc);
     $venue.append(venueContent);
   });
   $destination.append(`<h2>${venues[0].location.city}</h2>`);
 }
 
-const renderForecast = (days) => {
-  $weatherDivs.forEach(($day, index) => {
-    const currentDay = days[index];
-    let weatherContent = createWeatherHTML(currentDay);
-    $day.append(weatherContent);
-  });
+const renderForecast = (day) => {
+  const renderForecast = (day) => {
+  const weatherContent = createWeatherHTML(day);
+  $weatherDiv.append(weatherContent);
+};
+	let weatherContent = createWeatherHTML(day);
+  $weatherDiv.append(weatherContent);
 }
 
 const executeSearch = () => {
   $venueDivs.forEach(venue => venue.empty());
-  $weatherDivs.forEach(day => day.empty());
+  $weatherDiv.empty();
   $destination.empty();
   $container.css("visibility", "visible");
   getVenues().then(venues =>renderVenues(venues));
