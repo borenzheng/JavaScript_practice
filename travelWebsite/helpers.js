b569ebfc1f8a51e4080bf8f03c2c79b8a6e9da7e
borenzheng <boren_zheng@uri.edu>
2019-10-29 18:00:26 -0400

    update

--- travelWebsite/helpers.js
+++ travelWebsite/helpers.js
@@ -1,16 +1,19 @@
-// The code is provided by Codecademy learning platform
+// This file is provided by Codecademy learning platform
 const createVenueHTML = (name, location, iconSource) => {
-    return `<h2>${name}</h2>
-    <img class="venueimage" src="${iconSource}"/>
-    <h3>Address:</h3>
-    <p>${location.address}</p>
-    <p>${location.city}</p>
-    <p>${location.country}</p>`;
-  }
-  
-  const createWeatherHTML = (currentDay) => {
-    return `<h2> High: ${currentDay.day.maxtemp_f}</h2>
-      <h2> Low: ${currentDay.day.mintemp_f}</h2>
-      <img src="https://${currentDay.day.condition.icon}" class="weathericon" />
-      <h2>${weekDays[(new Date(currentDay.date)).getDay()]}</h2>`;
-  }
+  return `<h2>${name}</h2>
+  <img class="venueimage" src="${iconSource}"/>
+  <h3>Address:</h3>
+  <p>${location.address}</p>
+  <p>${location.city}</p>
+  <p>${location.country}</p>`;
+}
+
+const createWeatherHTML = (currentDay) => {
+  console.log(currentDay)
+  return `<h2>${weekDays[(new Date()).getDay()]}</h2>
+		<h2>Temperature: ${kelvinToFahrenheit(currentDay.main.temp)}&deg;F</h2>
+		<h2>Condition: ${currentDay.weather[0].description}</h2>
+  	<img src="https://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png">`;
+}
+
+const kelvinToFahrenheit = k => ((k - 273.15) * 9 / 5 + 32).toFixed(0);
